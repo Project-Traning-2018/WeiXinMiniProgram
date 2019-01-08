72a06284b2d62d767aac2ab3880e673a78e45ca0
JGC-HUST <a1344143728@163.com>
2019-01-08 18:37:20 +0800

    update

---
+++ prototype1/my-project/src/pages/home/home.vue
@@ -0,0 +1,73 @@
+<template id="tpl">
+  <div id="container">
+    <!--地图容器
+    longitude="poi.longitude"
+         latitude="poi.latitude"-->
+    <!--longitude及latitude为设置为调转到指定地址位置，默认不显示-->
+    <map id="myMap"
+         :markers="markers"
+
+         scale='16' show-location>
+    </map>
+    <!--form表单-->
+    <!--form表单-->
+    <!--<form @submit="formSubmit($event)">-->
+    <!--&lt;!&ndash;地址描述输入框,示例：北京市海淀区彩和坊路海淀西大街74号&ndash;&gt;-->
+    <!--<input style="border:1px solid #000;" name="geocoder"></input>-->
+    <!--&lt;!&ndash;提交表单数据&ndash;&gt;-->
+    <!--<button form-type="submit" bindtap="geo">地址解析</button>-->
+    <!--</form>-->
+    <cover-view>
+      <button @tap="handleTap">
+        发起投票
+      </button>
+    </cover-view>
+    <!--地址描述经纬度展示-->
+    <!--<view>地址纬度："{{poi.latitude}}"</view>-->
+    <!--<view>地址经度："{{poi.longitude}}"</view>-->
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    data() {
+      return {}
+    },
+    methods: {
+      handleTap() {
+        console.log("Tapped!")
+      }
+    }
+  }
+
+
+</script>
+
+<style scoped>
+  page {
+    height: 100%;
+    width: 100%;
+  }
+
+  #container,
+  #myMap {
+    height: 100%;
+    width: 100%;
+  }
+  button {
+    width: 550rpx;
+    height: 90rpx;
+    margin-left: -275rpx;
+    position: fixed;
+    left: 50%;
+    bottom: 50rpx;
+    background-color: #2C8DF6
+  }
+  cover-view {
+    position: fixed;
+    width: 750rpx;
+    height: 200rpx;
+    bottom: 0;
+  }
+</style>
---
+++ prototype1/my-project/src/pages/home/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './home'
+import QQMapWX from '../../../static/js/qqmap-wx-jssdk'
+
+let qqmapsdk = new QQMapWX({
+  key: "TL5BZ-LKU3R-YM7WP-W4CQH-UBHJ7-T2F5Y" // 必填
+});
+const app = new Vue(App)
+app.$mount()
+app.qqmapsdk = qqmapsdk
